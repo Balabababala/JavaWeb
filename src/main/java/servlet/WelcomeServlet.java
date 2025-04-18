dfdb9231aaa1ab1508f8c3d70ba21594215355aa
Balabababala <139853702+Balabababala@users.noreply.github.com>
2025-04-18 11:29:08 +0800

    04181129

--- src/main/java/servlet/WelcomeServlet.java
+++ src/main/java/servlet/WelcomeServlet.java
@@ -8,20 +8,22 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = {"/welcome"})
-//@WebServlet("/welcome")
-public class WelcomeServlet extends HttpServlet{
+@WebServlet("/welcome")
+public class WelcomeServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		// 設定編碼
 		resp.setCharacterEncoding("UTF-8");
 		resp.setContentType("text/html;charset=UTF-8");
-		resp.getWriter().print("歡迎光臨");
 		
+		// 接收參數
+		// 例如: http://localhost:8080/JavaWeb/welcome?name=Jack
+		String name = req.getParameter("name");
+		
+		resp.getWriter().print(name + " 歡迎光臨");
 		
 	}
-
 	
 	
 }
