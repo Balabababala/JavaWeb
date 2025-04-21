7bf1bded74d0a5d193bdc1055e4ceec60b565174
Balabababala <139853702+Balabababala@users.noreply.github.com>
2025-04-21 15:18:29 +0800

    04210318

---
+++ case02/src/main/java/case03/PhoneTest.java
@@ -0,0 +1,10 @@
+package case03;
+
+public class PhoneTest {
+
+	public static void main(String[] args) {
+		PhonePlan phonePlan =new PhonePlan(15000.0,1.0);
+		System.out.printf(phonePlan.getPhoneType());
+	}
+
+}
