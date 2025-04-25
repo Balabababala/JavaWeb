0864961f711280d9f6335ec6e855a040ef8d5a1d
Balabababala <139853702+Balabababala@users.noreply.github.com>
2025-04-25 16:36:26 +0800

    04251636

--- case02/src/main/java/case02/CoffeeTest.java
+++
@@ -1,8 +0,0 @@
-package case02;
-
-public class CoffeeTest {
-	public static void main(String[] args) {
-		Coffee2 coffee =new Coffee2(50.0,100.0);
-		System.out.printf(coffee.getCoffeeType());
-	}
-}
