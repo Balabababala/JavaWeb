27d5d9f01085828d9521990a424bf1422afbd8bb
Balabababala <139853702+Balabababala@users.noreply.github.com>
2025-04-28 11:56:22 +0800

    04281156

--- JavaWebCart/src/main/java/cart/controller/UserRegisterServlet.java
+++ JavaWebCart/src/main/java/cart/controller/UserRegisterServlet.java
@@ -1,7 +1,8 @@
 package cart.controller;
 
 import java.io.IOException;
-
+import cart.service.*;
+import cart.service.Impl.UserRegisterServiceImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,7 +11,8 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/user/register")
 public class UserRegisterServlet extends HttpServlet{
-
+	private UserRegisterService userRegisterService=new UserRegisterServiceImpl();
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.getRequestDispatcher("/WEB-INF/view/cart/user_register.jsp").forward(req, resp);
@@ -18,8 +20,11 @@ public class UserRegisterServlet extends HttpServlet{
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-
+		String username=req.getParameter("username");
+		String password=req.getParameter("password");
+		String email =req.getParameter("email");
+		userRegisterService.addUser(username,password,email);
+		req.getRequestDispatcher("/WEB-INF/view/cart/login.jsp");
 		
 	}
 
