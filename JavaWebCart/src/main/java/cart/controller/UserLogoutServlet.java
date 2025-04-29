c3f031c8227f406992b078333354e4e3bb9750e8
Balabababala <139853702+Balabababala@users.noreply.github.com>
2025-04-29 11:09:45 +0800

    04291109

--- JavaWebCart/src/main/java/cart/controller/UserLogoutServlet.java
+++ JavaWebCart/src/main/java/cart/controller/UserLogoutServlet.java
@@ -7,12 +7,16 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/user/logout")
 public class UserLogoutServlet extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session =req.getSession();
+		session.invalidate();
+		
 		req.getRequestDispatcher("/WEB-INF/view/cart/user_login.jsp").forward(req, resp);
 	}
 
